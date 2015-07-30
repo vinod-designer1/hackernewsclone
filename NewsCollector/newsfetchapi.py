cbd586e44ac541c0b1684c42edaf7be368fbaa28
Vinod Y <vinod.designer1@gmail.com>
2015-07-31 00:48:44 +0530

    added class to fetch top n articles and model to store them

---
+++ NewsCollector/newsfetchapi.py
@@ -0,0 +1,95 @@
+import urllib2
+import httplib
+import logging
+import json
+
+
+# Standard instance for logger with __name__
+stdLogger = logging.getLogger(__name__)
+
+def urlopener(url):
+  """
+  Fetches the give url and handles exceptions if any while fetching
+  """
+  try: 
+    response = urllib2.urlopen(url)
+  except urllib2.HTTPError, e:
+    stdLogger.error('HTTPError = ' + str(e.code))
+  except urllib2.URLError, e:
+    stdLogger.error('URLError = ' + str(e.reason))
+  except httplib.HTTPException, e:
+    stdLogger.error('HTTPException')
+  except Exception:
+    import traceback
+    stdLogger.error('generic exception: ' + traceback.format_exc())
+
+  return response
+
+def is_json(myjson):
+  try:
+    json_object = json.loads(myjson)
+  except ValueError, e:
+    return False
+  return True
+
+
+class HackerNewsFetcher():
+
+  def __init__(self, n):
+    self.n = n
+  
+  def get_item_details(self, item_id):
+    """
+    In hackernews Stories, comments, jobs, Ask HNs and even polls are just items. 
+    They're identified by their ids, which are unique integers, 
+    and can be fetched using https://hacker-news.firebaseio.com/v0/item/<item_id>.json?print=pretty.
+    """
+    url = "https://hacker-news.firebaseio.com/v0/item/%s.json?print=pretty" % (str(item_id),)
+    response = urlopener(url)
+    item_details = {}
+
+    if response:
+      data = response.read()
+      if is_json(data):
+        item_details = json.loads(data)
+
+    return item_details
+
+  def get_n_topstory_ids(self):
+    """
+    This method returns ids of top n stories in hackernews
+    we can fetch them from 
+    https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+    """
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+    response = urlopener(url)
+    list_of_top_story_ids = []
+
+    if response:
+      data = response.read()
+      if is_json(data):
+        list_of_top_story_ids = json.loads(data)
+
+    if len(list_of_top_story_ids) > self.n:
+      list_of_top_story_ids = list_of_top_story_ids[:self.n]
+
+    return list_of_top_story_ids
+
+  def get_n_topstory_details(self):
+    """
+    This method returns list of to n top stories in hackernews along with following details
+    id, posted_by, url, upvotes, comments, title, posted_on(time)
+    """
+    # Get n top story ids
+    ids_of_top_n_stories = self.get_n_topstory_ids()
+
+    top_n_story_details = []
+
+    # iterate over ids and get story details for each
+    for story_id in ids_of_top_n_stories:
+      story_details = self.get_item_details(story_id)
+
+      if story_details:
+        top_n_story_details.append(story_details)
+
+    return top_n_story_details
